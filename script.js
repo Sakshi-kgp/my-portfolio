91b9eec30e75aaed23839f8ca896e0c3f4ab0a14
Sakshi-kgp <sakshikumari.319.10b@gmail.com>
2025-06-27 16:40:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+  var typed = new Typed(".word", {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+
+
